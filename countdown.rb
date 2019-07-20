32722446a9c1bd0d8696ac6721f8d723905f850d
Renee France <renee5.0@outlook.com>
2019-07-20 14:19:17 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,11 +1,11 @@
 #write your code here
 
-def countdown (number = 10)
+def countdown(number = 10)
   while number > 0
   puts "#{number} SECOND(S)!"
   number -= 1
   end
-  puts "Happy NEW YEAR!"
+  return "Happy NEW YEAR!"
 end
 
 
